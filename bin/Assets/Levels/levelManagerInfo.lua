ec268cf23b1543a41fa5a79483269b184cf50be3
MarcoIMH <47715985+MarcoIMH@users.noreply.github.com>
2021-05-19 18:27:34 +0200

    Rondas predefinidas activas

--- bin/Assets/Levels/levelManagerInfo.lua
+++ bin/Assets/Levels/levelManagerInfo.lua
@@ -1,6 +1,6 @@
 -- prefab[1] = ROUND1 CONFIG, prefab[2] = ROUND2 CONFIG... prefab[N] = ROUNDN CONFIG
 prefab={}
-prefab[0] = { HowManyRespawnPositions = 4, HowManyRounds = 1, RespawnPositions = {{X=3,Y=0.5,Z=1.5}, {X=-3,Y=0.5,Z=-1.5}, {X=-3,Y=0.5,Z=1.5}, {X=3,Y=0.5,Z=-1.5}}}
+prefab[0] = { HowManyRespawnPositions = 4, HowManyRounds = 10, RespawnPositions = {{X=3,Y=0.5,Z=1.5}, {X=-3,Y=0.5,Z=-1.5}, {X=-3,Y=0.5,Z=1.5}, {X=3,Y=0.5,Z=-1.5}}}
 prefab[1] = { TotalEnemies = 10, WaveTime = 6,
 			  Granade    = { HowMany = 1, RespawnTime = 2}, 
 			  Lemon      = { HowMany = 0, RespawnTime = 2}, 
@@ -41,7 +41,3 @@ prefab[10] ={ TotalEnemies = 1, WaveTime = 3,
 			  Granade    = { HowMany = 1, RespawnTime = 2}, 
 			  Lemon      = { HowMany = 0, RespawnTime = 2}, 
 			  Watermelon = { HowMany = 0, RespawnTime = 2}} 
-prefab[11] = { TotalEnemies = 1, WaveTime = 3,
-			  Granade    = { HowMany = 0, RespawnTime = 1}, 
-			  Lemon      = { HowMany = 0, RespawnTime = 1}, 
-			  Watermelon = { HowMany = 1, RespawnTime = 1}} 
